ff90f8aabf0d82f1008140c567756460c22e90f5
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2015-02-27 13:46:39 -0700

    Simplifying script (now that stupid issue in CI is fixed)

--- run-perf.sh
+++ run-perf.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 
-echo "## User is `id`"
-echo "## Uname is `uname -a`"
 echo "## PWD is `pwd`"
-
 echo "## JAVA_HOME is ${JAVA_HOME}"
-JAVA_EXEC="${JAVA_HOME}/bin/java"
-if [ -e "${JAVA_EXEC}" ] ; then
-    echo "  Exec Exists: ${JAVA_EXEC}"
-    ls -la "${JAVA_EXEC}"
-else
-    echo "  Exec Missing: ${JAVA_EXEC}"
-fi
-
 echo "## M2_HOME is ${M2_HOME}"
-M2_EXEC="${M2_HOME}/bin/mvn"
-if [ -e "${M2_EXEC}" ] ; then
-    echo "  Exec Exists: ${M2_EXEC}"
-    ls -la "${M2_EXEC}"
-else
-    echo "  Exec Missing: ${M2_EXEC}"
-fi
-
-echo "## PATH is..."
-echo -n "  "
-echo $PATH | sed -e "s/:/\n  /g"
-
 echo "## Maven Version"
 mvn --version
 
