219cb5378f7d8aed5a178727804d2282bdbe7231
Ludovic Orban <lorban@bitronix.be>
2021-04-19 18:46:00 +0200

    comment out ssl test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- src/test/java/perf/SslPerfTest.java
+++ src/test/java/perf/SslPerfTest.java
@@ -39,168 +39,168 @@ import org.mortbay.jetty.orchestrator.util.IOUtil;
 
 public class SslPerfTest implements Serializable
 {
-    public static final int WARMUP_REQUEST_COUNT = 1_500_000;
-    public static final int RUN_REQUEST_COUNT = 3_000_000;
-
-    @Disabled
-    @Test
-    public void testSslPerf() throws Exception
-    {
-        installAsyncProfilerIfNeeded();
-
-        ClusterConfiguration cfg = new SimpleClusterConfiguration()
-            .jvm(new Jvm(() -> "java", "-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints"))
-            .nodeArray(new SimpleNodeArrayConfiguration("server-array").topology(new NodeArrayTopology(new Node("1", "localhost"))))
-            .nodeArray(new SimpleNodeArrayConfiguration("client-array").topology(new NodeArrayTopology(new Node("1", "localhost"))))
-            ;
-
-        try (Cluster cluster = new Cluster("NewSslPerfTest::sslPerf", cfg))
-        {
-            System.out.println("Initializing...");
-            NodeArray serverArray = cluster.nodeArray("server-array");
-            NodeArray clientArray = cluster.nodeArray("client-array");
-
-            serverArray.executeOnAll(tools ->
-            {
-                Server server = new Server();
-
-                HttpConnectionFactory http = new HttpConnectionFactory();
-
-                SslContextFactory.Server serverSslContextFactory = new SslContextFactory.Server();
-                String path = getClass().getResource("/keystore.p12").getPath();
-                serverSslContextFactory.setKeyStorePath(path);
-                serverSslContextFactory.setKeyStorePassword("storepwd");
-                SslConnectionFactory ssl = new SslConnectionFactory(serverSslContextFactory, http.getProtocol());
-
-                ServerConnector serverConnector = new ServerConnector(server, ssl, http);
-                serverConnector.setPort(8443);
-                server.addConnector(serverConnector);
-                server.setHandler(new AsyncHandler("Hi there!".getBytes(StandardCharsets.ISO_8859_1)));
-                server.start();
-
-                tools.atomicCounter("server-pid", ProcessHandle.current().pid());
-            }).get();
-            long serverPid = cluster.tools().atomicCounter("server-pid", 0L).get();
-
-            System.out.println("Warming up...");
-            clientArray.executeOnAll(tools -> runClient(WARMUP_REQUEST_COUNT, "https://localhost:8443")).get();
-
-            System.out.println("Running...");
-            long before = System.nanoTime();
-            NodeArrayFuture clientArrayFuture = clientArray.executeOnAll(tools ->
-            {
-                tools.atomicCounter("client-pid", ProcessHandle.current().pid());
-                tools.barrier("client-barrier", 2).await();
-                tools.barrier("client-barrier", 2).await();
-
-                runClient(RUN_REQUEST_COUNT, "https://localhost:8443");
-            });
-
-            cluster.tools().barrier("client-barrier", 2).await();
-            long clientPid = cluster.tools().atomicCounter("client-pid", 0L).get();
-            spawnAsyncProfiler("target/server.html", serverPid);
-            spawnAsyncProfiler("target/client.html", clientPid);
-            cluster.tools().barrier("client-barrier", 2).await();
-            clientArrayFuture.get();
-
-            long after = System.nanoTime();
-            System.out.println("Done; elapsed=" + TimeUnit.NANOSECONDS.toMillis(after - before) + " ms");
-        }
-    }
-
-    private void runClient(int count, String urlAsString) throws Exception
-    {
-        long before = System.nanoTime();
-        System.out.println("Running client; " + count + " requests...");
-        URI uri = new URI(urlAsString);
-
-        SslContextFactory.Client clientSslContextFactory = new SslContextFactory.Client();
-        String path = getClass().getResource("/keystore.p12").getPath();
-        clientSslContextFactory.setKeyStorePath(path);
-        clientSslContextFactory.setKeyStorePassword("storepwd");
-
-        ClientConnector clientConnector = new ClientConnector();
-        clientConnector.setSslContextFactory(clientSslContextFactory);
-
-        HttpClientTransportOverHTTP transport = new HttpClientTransportOverHTTP(clientConnector);
-        HttpClient httpClient = new HttpClient(transport);
-        httpClient.setCookieStore(new HttpCookieStore.Empty());
-        httpClient.start();
-
-        CountDownLatch latch = new CountDownLatch(count);
-        for (int i = 0; i < count; i++)
-        {
-            httpClient.newRequest(uri).send(r -> latch.countDown());
-        }
-        latch.await();
-
-        httpClient.stop();
-        long elapsedSeconds = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - before);
-        System.out.println("Stopped client; ran for " + elapsedSeconds + " seconds");
-    }
-
-    private static void installAsyncProfilerIfNeeded() throws IOException
-    {
-        File asyncProfilerHome = new File("target/async-profiler-2.0-linux-x64");
-        if (!asyncProfilerHome.isDirectory())
-        {
-            System.out.println("installing async profiler...");
-            File tarGzFile = new File("target/async-profiler-2.0-linux-x64.tar.gz");
-            try (InputStream is = new URL("https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.0/async-profiler-2.0-linux-x64.tar.gz").openStream();
-                 OutputStream os = new FileOutputStream(tarGzFile))
-            {
-                IOUtil.copy(is, os);
-            }
-            TarGZipUnArchiver ua = new TarGZipUnArchiver(tarGzFile);
-            ua.enableLogging(new AbstractLogger(0, "") {
-                @Override
-                public void debug(String s, Throwable throwable)
-                {
-
-                }
-
-                @Override
-                public void info(String s, Throwable throwable)
-                {
-
-                }
-
-                @Override
-                public void warn(String s, Throwable throwable)
-                {
-
-                }
-
-                @Override
-                public void error(String s, Throwable throwable)
-                {
-
-                }
-
-                @Override
-                public void fatalError(String s, Throwable throwable)
-                {
-
-                }
-
-                @Override
-                public Logger getChildLogger(String s)
-                {
-                    return null;
-                }
-            });
-            ua.setDestDirectory(tarGzFile.getParentFile());
-            ua.extract();
-        }
-    }
-
-    private static void spawnAsyncProfiler(String fgFilename, long pid) throws IOException
-    {
-        File asyncProfilerHome = new File("target/async-profiler-2.0-linux-x64");
-        File fgFile = new File(fgFilename);
-        new ProcessBuilder("./profiler.sh", "-d", "3600", "-f", fgFile.getAbsolutePath(), Long.toString(pid))
-            .directory(asyncProfilerHome)
-            .redirectErrorStream(true)
-            .start();
-    }
+//    public static final int WARMUP_REQUEST_COUNT = 1_500_000;
+//    public static final int RUN_REQUEST_COUNT = 3_000_000;
+//
+//    @Disabled
+//    @Test
+//    public void testSslPerf() throws Exception
+//    {
+//        installAsyncProfilerIfNeeded();
+//
+//        ClusterConfiguration cfg = new SimpleClusterConfiguration()
+//            .jvm(new Jvm(() -> "java", "-XX:+UnlockDiagnosticVMOptions", "-XX:+DebugNonSafepoints"))
+//            .nodeArray(new SimpleNodeArrayConfiguration("server-array").topology(new NodeArrayTopology(new Node("1", "localhost"))))
+//            .nodeArray(new SimpleNodeArrayConfiguration("client-array").topology(new NodeArrayTopology(new Node("1", "localhost"))))
+//            ;
+//
+//        try (Cluster cluster = new Cluster("NewSslPerfTest::sslPerf", cfg))
+//        {
+//            System.out.println("Initializing...");
+//            NodeArray serverArray = cluster.nodeArray("server-array");
+//            NodeArray clientArray = cluster.nodeArray("client-array");
+//
+//            serverArray.executeOnAll(tools ->
+//            {
+//                Server server = new Server();
+//
+//                HttpConnectionFactory http = new HttpConnectionFactory();
+//
+//                SslContextFactory.Server serverSslContextFactory = new SslContextFactory.Server();
+//                String path = getClass().getResource("/keystore.p12").getPath();
+//                serverSslContextFactory.setKeyStorePath(path);
+//                serverSslContextFactory.setKeyStorePassword("storepwd");
+//                SslConnectionFactory ssl = new SslConnectionFactory(serverSslContextFactory, http.getProtocol());
+//
+//                ServerConnector serverConnector = new ServerConnector(server, ssl, http);
+//                serverConnector.setPort(8443);
+//                server.addConnector(serverConnector);
+//                server.setHandler(new AsyncHandler("Hi there!".getBytes(StandardCharsets.ISO_8859_1)));
+//                server.start();
+//
+//                tools.atomicCounter("server-pid", ProcessHandle.current().pid());
+//            }).get();
+//            long serverPid = cluster.tools().atomicCounter("server-pid", 0L).get();
+//
+//            System.out.println("Warming up...");
+//            clientArray.executeOnAll(tools -> runClient(WARMUP_REQUEST_COUNT, "https://localhost:8443")).get();
+//
+//            System.out.println("Running...");
+//            long before = System.nanoTime();
+//            NodeArrayFuture clientArrayFuture = clientArray.executeOnAll(tools ->
+//            {
+//                tools.atomicCounter("client-pid", ProcessHandle.current().pid());
+//                tools.barrier("client-barrier", 2).await();
+//                tools.barrier("client-barrier", 2).await();
+//
+//                runClient(RUN_REQUEST_COUNT, "https://localhost:8443");
+//            });
+//
+//            cluster.tools().barrier("client-barrier", 2).await();
+//            long clientPid = cluster.tools().atomicCounter("client-pid", 0L).get();
+//            spawnAsyncProfiler("target/server.html", serverPid);
+//            spawnAsyncProfiler("target/client.html", clientPid);
+//            cluster.tools().barrier("client-barrier", 2).await();
+//            clientArrayFuture.get();
+//
+//            long after = System.nanoTime();
+//            System.out.println("Done; elapsed=" + TimeUnit.NANOSECONDS.toMillis(after - before) + " ms");
+//        }
+//    }
+//
+//    private void runClient(int count, String urlAsString) throws Exception
+//    {
+//        long before = System.nanoTime();
+//        System.out.println("Running client; " + count + " requests...");
+//        URI uri = new URI(urlAsString);
+//
+//        SslContextFactory.Client clientSslContextFactory = new SslContextFactory.Client();
+//        String path = getClass().getResource("/keystore.p12").getPath();
+//        clientSslContextFactory.setKeyStorePath(path);
+//        clientSslContextFactory.setKeyStorePassword("storepwd");
+//
+//        ClientConnector clientConnector = new ClientConnector();
+//        clientConnector.setSslContextFactory(clientSslContextFactory);
+//
+//        HttpClientTransportOverHTTP transport = new HttpClientTransportOverHTTP(clientConnector);
+//        HttpClient httpClient = new HttpClient(transport);
+//        httpClient.setCookieStore(new HttpCookieStore.Empty());
+//        httpClient.start();
+//
+//        CountDownLatch latch = new CountDownLatch(count);
+//        for (int i = 0; i < count; i++)
+//        {
+//            httpClient.newRequest(uri).send(r -> latch.countDown());
+//        }
+//        latch.await();
+//
+//        httpClient.stop();
+//        long elapsedSeconds = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - before);
+//        System.out.println("Stopped client; ran for " + elapsedSeconds + " seconds");
+//    }
+//
+//    private static void installAsyncProfilerIfNeeded() throws IOException
+//    {
+//        File asyncProfilerHome = new File("target/async-profiler-2.0-linux-x64");
+//        if (!asyncProfilerHome.isDirectory())
+//        {
+//            System.out.println("installing async profiler...");
+//            File tarGzFile = new File("target/async-profiler-2.0-linux-x64.tar.gz");
+//            try (InputStream is = new URL("https://github.com/jvm-profiling-tools/async-profiler/releases/download/v2.0/async-profiler-2.0-linux-x64.tar.gz").openStream();
+//                 OutputStream os = new FileOutputStream(tarGzFile))
+//            {
+//                IOUtil.copy(is, os);
+//            }
+//            TarGZipUnArchiver ua = new TarGZipUnArchiver(tarGzFile);
+//            ua.enableLogging(new AbstractLogger(0, "") {
+//                @Override
+//                public void debug(String s, Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void info(String s, Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void warn(String s, Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void error(String s, Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void fatalError(String s, Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public Logger getChildLogger(String s)
+//                {
+//                    return null;
+//                }
+//            });
+//            ua.setDestDirectory(tarGzFile.getParentFile());
+//            ua.extract();
+//        }
+//    }
+//
+//    private static void spawnAsyncProfiler(String fgFilename, long pid) throws IOException
+//    {
+//        File asyncProfilerHome = new File("target/async-profiler-2.0-linux-x64");
+//        File fgFile = new File(fgFilename);
+//        new ProcessBuilder("./profiler.sh", "-d", "3600", "-f", fgFile.getAbsolutePath(), Long.toString(pid))
+//            .directory(asyncProfilerHome)
+//            .redirectErrorStream(true)
+//            .start();
+//    }
 }
