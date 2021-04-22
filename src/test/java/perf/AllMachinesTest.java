1309951c975b2c0ad1dfb37298856b062dc9db08
Ludovic Orban <lorban@bitronix.be>
2021-04-22 09:13:14 +0200

    us jenkins jdk tool convention to pick JDK
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- src/test/java/perf/AllMachinesTest.java
+++ src/test/java/perf/AllMachinesTest.java
@@ -1,5 +1,7 @@
 package perf;
 
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 import org.mortbay.jetty.orchestrator.Cluster;
 import org.mortbay.jetty.orchestrator.NodeArray;
@@ -11,16 +13,15 @@ import org.mortbay.jetty.orchestrator.configuration.NodeArrayTopology;
 import org.mortbay.jetty.orchestrator.configuration.SimpleClusterConfiguration;
 import org.mortbay.jetty.orchestrator.configuration.SimpleNodeArrayConfiguration;
 import org.mortbay.jetty.orchestrator.configuration.SshRemoteHostLauncher;
+import org.mortbay.jetty.orchestrator.util.SerializableSupplier;
 
 public class AllMachinesTest
 {
     @Test
     public void testAllMachines() throws Exception
     {
-        String javaHome = System.getProperty("remote.java.home", System.getenv("JAVA_HOME"));
-        String jenkinsHome = System.getenv("JENKINS_HOME");
         ClusterConfiguration cfg = new SimpleClusterConfiguration()
-            .jvm(new Jvm(() -> javaHome + "/bin/java"))
+            .jvm(new Jvm(new JenkinsJdkTool("jdk11")))
             .hostLauncher(new SshRemoteHostLauncher())
             .nodeArray(new SimpleNodeArrayConfiguration("server").topology(new NodeArrayTopology(
                 new Node("1", "load-master")
@@ -38,7 +39,7 @@ public class AllMachinesTest
         {
             String javaVersion = System.getProperty("java.version");
             String username = System.getProperty("user.name");
-            System.out.println("jenkins running java " + javaVersion + " with user " + username + " and JAVA_HOME=" + javaHome);
+            System.out.println("jenkins running java " + javaVersion + " with user " + username);
         }
 
         try (Cluster cluster = new Cluster(cfg))
@@ -69,6 +70,30 @@ public class AllMachinesTest
             serverFuture.get();
             loadersFuture.get();
             probeFuture.get();
+        }
+    }
+
+    private static class JenkinsJdkTool implements SerializableSupplier<String>
+    {
+        private final String toolName;
+
+        private JenkinsJdkTool(String toolName)
+        {
+            this.toolName = toolName;
+        }
+
+        @Override
+        public String get()
+        {
+            String home = System.getProperty("user.home");
+            File jdkFolderFile = new File(home + "/jenkins_home/tools/hudson.model.JDK/" + toolName);
+            if (!jdkFolderFile.isDirectory())
+                throw new RuntimeException("Jenkins tool '" + toolName + "' not installed");
+            File[] files = jdkFolderFile.listFiles();
+            if (files == null || files.length == 0)
+                throw new RuntimeException("Jenkins tool '" + toolName + "' not found");
+            File executableFile = new File(files[0], "bin/java");
+            return executableFile.getAbsolutePath();
         }
     }
 }
