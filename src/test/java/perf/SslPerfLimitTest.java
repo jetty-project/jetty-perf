5c3ad0ba14773c9035e3887e4ab32988f2853f32
Ludovic Orban <lorban@bitronix.be>
2021-05-07 17:33:50 +0200

    disable load-2

--- src/test/java/perf/SslPerfLimitTest.java
+++ src/test/java/perf/SslPerfLimitTest.java
@@ -84,7 +84,7 @@ public class SslPerfLimitTest implements Serializable
             )
             .nodeArray(new SimpleNodeArrayConfiguration("loaders")
                 .node(new Node("1", "load-1"))
-                .node(new Node("2", "load-2"))
+                //.node(new Node("2", "load-2"))
                 .node(new Node("3", "load-3"))
                 .node(new Node("4", "load-4"))
             )
