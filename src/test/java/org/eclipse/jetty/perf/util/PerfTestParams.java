59bc54a25923f81ac88d947de99dd8e5463d0e72
Ludovic Orban <lorban@bitronix.be>
2021-10-05 18:58:01 +0200

    adjust expected H2 P99 latency

--- src/test/java/org/eclipse/jetty/perf/util/PerfTestParams.java
+++ src/test/java/org/eclipse/jetty/perf/util/PerfTestParams.java
@@ -154,7 +154,7 @@ public class PerfTestParams implements Serializable
             case h2c:
                 return 13_000;
             case h2:
-                return 45_000;
+                return 20_000;
             default:
                 throw new AssertionError();
         }
