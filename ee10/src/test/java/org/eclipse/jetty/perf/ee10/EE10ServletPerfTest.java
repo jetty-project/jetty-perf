058f06f9ce71376f813f4a0ed7a971fa82b49564
Ludovic Orban <lorban@bitronix.be>
2024-02-07 17:15:52 +0100

    add EE10 gzip test

--- ee10/src/test/java/org/eclipse/jetty/perf/ee10/EE10ServletPerfTest.java
+++ ee10/src/test/java/org/eclipse/jetty/perf/ee10/EE10ServletPerfTest.java
@@ -7,6 +7,7 @@ import org.eclipse.jetty.perf.test.Jetty12ClusteredPerfTest;
 import org.eclipse.jetty.perf.test.ClusteredTestContext;
 import org.eclipse.jetty.perf.test.junit.ClusteredTest;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.junit.jupiter.api.Test;
 
 public class EE10ServletPerfTest
@@ -44,6 +45,26 @@ public class EE10ServletPerfTest
             uselessContextHandler.addServlet(new Always404Servlet(), "/*");
             contextHandlerCollection.addHandler(uselessContextHandler);
             return contextHandlerCollection;
+        });
+    }
+
+    @Test
+    public void testGzipSync(@ClusteredTest ClusteredTestContext clusteredTestContext) throws Exception
+    {
+        Jetty12ClusteredPerfTest.runTest(clusteredTestContext, () ->
+        {
+            ContextHandlerCollection contextHandlerCollection = new ContextHandlerCollection();
+            ServletContextHandler targetContextHandler = new ServletContextHandler();
+            targetContextHandler.setContextPath("/");
+            targetContextHandler.addServlet(new SyncEE10Servlet("Hi there!".getBytes(StandardCharsets.ISO_8859_1)), "/*");
+            contextHandlerCollection.addHandler(targetContextHandler);
+            ServletContextHandler uselessContextHandler = new ServletContextHandler();
+            uselessContextHandler.setContextPath("/useless");
+            uselessContextHandler.addServlet(new Always404Servlet(), "/*");
+            contextHandlerCollection.addHandler(uselessContextHandler);
+            GzipHandler gzipHandler = new GzipHandler(contextHandlerCollection);
+            gzipHandler.setMinGzipSize(1);
+            return gzipHandler;
         });
     }
 }
