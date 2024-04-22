bd77e3b764d087239a5c71aa671a51f00ab8941b
Olivier Lamy <olamy@apache.org>
2024-04-22 04:01:37 +0200

    Update toolchains.groovy

--- vars/toolchains.groovy
+++ vars/toolchains.groovy
@@ -2,5 +2,10 @@
 
 def call(Map params = [:]) {
 
+  def jdkToUse = params.containsKey("jdkToUse") ? params.branchesToNotify : '' // exception if empty?
+
+  jdkpathfinder nodes: ['load-master-2', 'load-2', 'load-5', 'load-3', 'load-6', 'load-sample'],
+                jdkNames: ["${JDK_TO_USE}"]
+  stash name: 'toolchains.xml', includes: '*toolchains.xml'
   
 }
