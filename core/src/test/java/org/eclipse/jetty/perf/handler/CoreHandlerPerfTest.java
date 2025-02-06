ae26e923144753450da32b22fc6a59ac73cb0337
Ludovic Orban <lorban@bitronix.be>
2025-02-06 13:35:27 +0100

    adapt load latencies to new env
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- core/src/test/java/org/eclipse/jetty/perf/handler/CoreHandlerPerfTest.java
+++ core/src/test/java/org/eclipse/jetty/perf/handler/CoreHandlerPerfTest.java
@@ -102,7 +102,7 @@ public class CoreHandlerPerfTest
 
     @ParameterizedTest(name = "{0}")
     @CsvSource({
-        "http, 60_000, 2_500, 800_000, 15.0",
+        "http, 60_000, 3_000, 800_000, 15.0",
         "h2c,  60_000, 8_000, 850_000, 15.0"
     })
     public void testNoGzipFullyAsyncHandlerTree(PerfTestParams.Protocol protocol, int loaderRate, long expectedP99ServerLatency, long expectedP99ProbeLatency, double expectedP99ErrorMargin) throws Exception
